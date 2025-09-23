31a55dded8711227fb3f2c2aaa643c14ccdb5aca
Amarjeet-05 <ajbhardwaj2005@gmail.com>
2025-09-23 18:53:12 +0530

    Update CodeLanConv.py

--- CodeLanConv.py
+++ CodeLanConv.py
@@ -5,8 +5,8 @@ split = n.split(" ")
 length = 3
 new = [] #empty list is made for storing the words in a list
 code = input("1 for coding and 0 for decoding : ")
-code = True if(code=='1') else False
-if(code):
+# code = True if(code=='1') else False
+if(code == '1'):
     for ele in split:
         if(len(ele) >= 3):
             r1 = ''.join(random.choices(string.ascii_letters, k=length))
@@ -19,7 +19,7 @@ if(code):
 # print(new)   #it print in the form of a list
     print(" ".join(new)) #The join() method iterates through the elements of the provided iterable and combines them into a single string, inserting the separator string between each element.
 
-else:
+elif(code == '0'):
     for i in split:
         if(len(i) >= 3):
             str = i[3:-3] #this is used to remove first and last three characters
@@ -27,4 +27,8 @@ else:
             new.append(str)
         else:
             new.append(i[::-1])
+
     print(" ".join(new))
+
+else:
+    print("Invalid Input!")
